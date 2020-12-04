539084e815213111a7a07733305cbd85ee46f527
Walt Panfil <panfilwk@rose-hulman.edu>
2020-12-03 22:20:31 -0500

    Solve Day 3 Part 2 Golang

--- day3/main.go
+++ day3/main.go
@@ -13,13 +13,7 @@ func check(e error) {
   }
 }
 
-func main() {
-  defer func() {
-    if r := recover(); r != nil {
-      log.Fatal(recover())
-    }
-  }()
-
+func tree_count(right, down int) int {
   file, err := os.Open("input.txt")
   check(err)
   defer file.Close()
@@ -31,12 +25,27 @@ func main() {
     if width == -1 {
       width = len(scanner.Text())
     }
-    if []rune(scanner.Text())[3*idx % width] == '#' {
-      fmt.Println("Hit tree on row", idx)
+    if idx % down == 0 && []rune(scanner.Text())[right*idx/down % width] == '#' {
       count++
     }
     idx++
   }
   check(scanner.Err())
-  fmt.Println(count)
+  return count
+}
+
+func main() {
+  defer func() {
+    if r := recover(); r != nil {
+      log.Fatal(recover())
+    }
+  }()
+
+  count11 := tree_count(1, 1)
+  count31 := tree_count(3, 1)
+  count51 := tree_count(5, 1)
+  count71 := tree_count(7, 1)
+  count12 := tree_count(1, 2)
+  fmt.Println("On the \"Right 3, Down 1\" path, you hit", count31, "trees")
+  fmt.Println("Product of all paths", count11*count31*count51*count71*count12)
 }
